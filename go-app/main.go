be74a809dac557f0a3f26d9b887deebfdbac3d2e
PC1412\quych <caohoanghuy2000@gmail.com>
2023-12-22 21:18:02 +0700

    {init code base} fix docker file

--- go-app/main.go
+++ go-app/main.go
@@ -3,12 +3,15 @@ package main
 import (
 	"net/http"
 
+	"github.com/caoquy2000/meeting-app/infrastructure"
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
 	router := gin.Default()
 	router.GET("/", func(context *gin.Context) {
+		infrastructure.LoadEnv()
+		infrastructure.NewDatabase()
 		context.JSON(http.StatusOK, gin.H{"data": "Hello World"})
 	})
 	router.Run(":8000")
