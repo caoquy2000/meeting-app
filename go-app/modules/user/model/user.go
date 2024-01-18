cbe1fd948e7c5063471983cfac8198be1ba45da2
caoquy2000 <caohoanghuy2000@gmail.com>
2024-01-18 12:46:07 +0700

    {WIP} add user model

--- go-app/modules/user/model/user.go
+++ go-app/modules/user/model/user.go
@@ -1,7 +1,10 @@
 package usermodel
 
+import "github.com/caoquy2000/meeting-app/common"
+
 const EntityName = "User"
 
 type User struct {
-
+	common.SQLModel `json:",inline"`
+	Email           string `json:"email" gorm:"column:email;"`
 }
